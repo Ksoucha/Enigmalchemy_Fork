4472a84b1fffc92ca352db105960cede6b4d28e8
Brannigan CHATEAU <brannigan.pro@gmail.com>
2024-11-14 16:10:33 +0100

    Finished deplacement

--- NotAName/Assets/Scripts/Player/PlayerMovement.cs
+++ NotAName/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,38 +4,56 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
-    public float moveSpeed;
+    private float moveSpeed;
+    public float walkSpeed;
+    public float sprintSpeed;
     public float groundDrag;
 
     public float jumpForce;
     public float jumpCooldown;
     public float airMultiplier;
+  
     bool readyToJump;
     [Header("Keybinds")]
     public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode crounchKey = KeyCode.LeftControl;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+
+    [Header("Crouching")]
+    public float crouchSpeed;
+    public float crouchYScale;
+    private float startYScale;
+
     [Header("Ground Check")]
     public float playerHeight;
     public LayerMask whatIsGround;
     bool grounded;
     public Transform orientation;
-
     float horizontalInput;
     float verticalInput;
 
     Vector3 moveDirection;
 
     Rigidbody rb;
-
+    public MovementState state;
+    public enum MovementState{
+        walking,
+        sprinting,
+        air,
+        crounching
+    }
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
         readyToJump = true;
+        startYScale = transform.localScale.y;
     }
     void Update()
     {
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
         MyInput();
+        StateHandler();
         if (grounded)
             rb.linearDamping = groundDrag;
         else
@@ -51,7 +69,6 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
-        if(Input.GetKey(jumpKey))
         if(Input.GetKey(jumpKey) && readyToJump && grounded)
         {
 
@@ -61,8 +78,40 @@ public class PlayerMovement : MonoBehaviour
 
             Invoke(nameof(ResetJump), jumpCooldown);
         }
+        if(Input.GetKeyDown(crounchKey))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
+            rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+        }
+        if(Input.GetKeyUp(crounchKey))
+        {
+            transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
+        }
 
     }
+    private void StateHandler()
+    {
+        if(Input.GetKey(crounchKey))
+        {
+            state = MovementState.crounching;
+            moveSpeed = crouchSpeed;
+        }
+        if(grounded && Input.GetKey(sprintKey))
+        {
+            state = MovementState.sprinting;
+            moveSpeed = sprintSpeed;
+        }
+        else if (grounded)
+        {
+            state = MovementState.walking;
+            moveSpeed = walkSpeed;
+        }
+        else
+        {
+            state = MovementState.air;
+        }
+    }
+
     private void MovePlayer()
     {
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
