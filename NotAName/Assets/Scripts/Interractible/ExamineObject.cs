42874efc02e768f205fe6ec62c1585c3f4dc7463
Brannigan CHATEAU <brannigan.pro@gmail.com>
2024-11-28 18:53:45 +0100

    Fixed examine object

--- NotAName/Assets/Scripts/Interractible/ExamineObject.cs
+++ NotAName/Assets/Scripts/Interractible/ExamineObject.cs
@@ -1,11 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Net.NetworkInformation;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.UIElements;
 using Cursor = UnityEngine.Cursor;
 
 public class ExamineObject : MonoBehaviour
@@ -23,12 +16,11 @@ public class ExamineObject : MonoBehaviour
     private bool inRange = false;
     public Transform spawn;
 
-
-    //List of position and rotation of the interactble objects 
     private Vector3 originalPosition;
     private Quaternion originalRotation;
 
-
+    private Quaternion currentRotation;
+    private bool isDragging = false; 
 
     void Start()
     {
@@ -39,19 +31,20 @@ public class ExamineObject : MonoBehaviour
         transform.rotation = spawn.rotation;
         originalPosition = transform.position;
         originalRotation = transform.rotation;
+
+        currentRotation = transform.rotation;
     }
 
     void Update()
     {
         float distanceToPlayer = Vector3.Distance(player.position, transform.position);
-
-        if (distanceToPlayer <= pickUpRange) inRange = true;
-        else inRange = false;
+        inRange = distanceToPlayer <= pickUpRange;
 
         if (UserInput.Instance.InteractInput && inRange)
         {
             ToggleExamination();
-            if (isExamining) StartExamination();
+            if (isExamining)
+                StartExamination();
             else
             {
                 StopExamination();
@@ -59,13 +52,34 @@ public class ExamineObject : MonoBehaviour
             }
         }
 
-        if (UserInput.Instance.InspectInput && isExamining)
+        if (isExamining)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                lastMousePosition = Input.mousePosition;
+                isDragging = true;
+            }
+
+            if (Input.GetMouseButton(0) && isDragging)
+            {
+                Examine();
+            }
+
+            if (Input.GetMouseButtonUp(0) && isDragging)
+            {
+                isDragging = false;
+
+                currentRotation = transform.rotation;
+            }
+        }
+        else if (!isExamining && inRange)
+        {
+            _canva.enabled = true;
+        }
+        else
         {
             _canva.enabled = false;
-            Examine(); 
         }
-        else if (!isExamining && inRange) _canva.enabled = true;
-        else _canva.enabled = false;
     }
 
     public void ToggleExamination()
@@ -75,13 +89,14 @@ public class ExamineObject : MonoBehaviour
 
     void StartExamination()
     {
-        lastMousePosition = Input.mousePosition;
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
         playerObject.GetComponent<PlayerMovement>().enabled = false;
         camObject.GetComponent<PlayerCam>().enabled = false;
         GetComponent<Collider>().enabled = false;
         transform.position = offset.position;
+
+        transform.rotation = currentRotation;
     }
 
     void StopExamination()
@@ -95,19 +110,20 @@ public class ExamineObject : MonoBehaviour
 
     void Examine()
     {
-        // move object with mouse
         Vector3 deltaMouse = Input.mousePosition - lastMousePosition;
         float rotationSpeed = 0.2f;
+
         transform.Rotate(deltaMouse.x * rotationSpeed * Vector3.down, Space.World);
         transform.Rotate(deltaMouse.y * rotationSpeed * Vector3.left, Space.World);
+
         lastMousePosition = Input.mousePosition;
     }
-
 
     void NonExamine()
     {
         transform.position = spawn.position;
-        transform.rotation = spawn.rotation;
-    }
+        transform.rotation = originalRotation;
 
+        currentRotation = originalRotation;
+    }
 }
